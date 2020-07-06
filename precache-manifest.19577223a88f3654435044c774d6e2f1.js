9cf762855b1905c1c9fd13f4f70daffacaad4e64
PrettyCoffee <jonathan-holz@outlook.com>
2020-07-06 22:22:10 +0200

    initial commit

---
+++ precache-manifest.19577223a88f3654435044c774d6e2f1.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "97f2bfe14486b4fa7797b1b4ad810ae4",
+    "url": "./index.html"
+  },
+  {
+    "revision": "e01c89003051484dba8d",
+    "url": "./static/css/2.d34346ea.chunk.css"
+  },
+  {
+    "revision": "5f3bef713dfd2e4b0e5a",
+    "url": "./static/css/main.b2dad0e7.chunk.css"
+  },
+  {
+    "revision": "e01c89003051484dba8d",
+    "url": "./static/js/2.2977768a.chunk.js"
+  },
+  {
+    "revision": "5ac48c47bb3912b14c2d8de4f56d5ae8",
+    "url": "./static/js/2.2977768a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "5f3bef713dfd2e4b0e5a",
+    "url": "./static/js/main.36d9812d.chunk.js"
+  },
+  {
+    "revision": "7888f9ce2489bc40833b",
+    "url": "./static/js/runtime-main.ada85091.js"
+  },
+  {
+    "revision": "a9fbb495313e4cb2265a1284392e9da6",
+    "url": "./static/media/FuturaLTLight.a9fbb495.woff"
+  },
+  {
+    "revision": "ba1342f42d643e4f554eaf120c6a2a38",
+    "url": "./static/media/pic.ba1342f4.png"
+  }
+]);
